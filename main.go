8a8cc4ea31fe9a331e94257695d02dfdd783f316
Tamal Saha <tamal@appscode.com>
2022-07-09 01:39:00 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,4 +1,35 @@
 package main
 
-func main() {}
+type ImageManifest struct {
+	SchemaVersion int                     `json:"schemaVersion"`
+	MediaType     string                  `json:"mediaType"`
+	Manifests     []PlatformImageManifest `json:"manifests"`
+	Config        ImageConfig             `json:"config"`
+	Layers        []ImageLayer            `json:"layers"`
+}
 
+type PlatformImageManifest struct {
+	MediaType string   `json:"mediaType"`
+	Size      int      `json:"size"`
+	Digest    string   `json:"digest"`
+	Platform  Platform `json:"platform"`
+}
+
+type Platform struct {
+	Architecture string `json:"architecture"`
+	Os           string `json:"os"`
+}
+
+type ImageConfig struct {
+	MediaType string `json:"mediaType"`
+	Size      int    `json:"size"`
+	Digest    string `json:"digest"`
+}
+
+type ImageLayer struct {
+	MediaType string `json:"mediaType"`
+	Size      int    `json:"size"`
+	Digest    string `json:"digest"`
+}
+
+func main() {}
