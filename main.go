e914ca8ee0885ba2eaac66daca17a3ab9c2b25bd
Tamal Saha <tamal@appscode.com>
2022-07-13 07:49:42 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/google/go-containerregistry/pkg/crane"
 	"k8s.io/apimachinery/pkg/runtime"
 	"kmodules.xyz/client-go/tools/parser"
@@ -43,8 +42,176 @@ type ImageLayer struct {
 
 func main() {
 	dir := "/Users/tamal/go/src/kubedb.dev/installer/catalog/raw"
+
+	dm := map[string]int{}
+
 	err := parser.ProcessPath(dir, func(ri parser.ResourceInfo) error {
 		switch ri.Object.GetKind() {
+		case api.ResourceKindElasticsearchVersion:
+			var v api.ElasticsearchVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Dashboard.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.DashboardInitContainer.YQImage, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindMemcachedVersion:
+			var v api.MemcachedVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindMariaDBVersion:
+			var v api.MariaDBVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Coordinator.Image, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindMongoDBVersion:
+			var v api.MongoDBVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.ReplicationModeDetector.Image, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindMySQLVersion:
+			var v api.MySQLVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.ReplicationModeDetector.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Coordinator.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Router.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.RouterInitContainer.Image, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindPerconaXtraDBVersion:
+			var v api.PerconaXtraDBVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindPgBouncerVersion:
+			var v api.PgBouncerVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.PgBouncer.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindProxySQLVersion:
+			var v api.ProxySQLVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.Proxysql.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+		case api.ResourceKindRedisVersion:
+			var v api.RedisVersion
+			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.Coordinator.Image, dm); err != nil {
+				panic(err)
+			}
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
+			}
 		case api.ResourceKindPostgresVersion:
 			var v api.PostgresVersion
 			err := runtime.DefaultUnstructuredConverter.FromUnstructured(ri.Object.UnstructuredContent(), &v)
@@ -52,31 +219,45 @@ func main() {
 				panic(err)
 			}
 
-			data, err := crane.Manifest(v.Spec.DB.Image)
-			if err != nil {
+			if err := collect(v.Spec.DB.Image, dm); err != nil {
 				panic(err)
 			}
-
-			var m ImageManifest
-			err = json.Unmarshal(data, &m)
-			if err != nil {
+			if err := collect(v.Spec.Coordinator.Image, dm); err != nil {
 				panic(err)
 			}
-
-			for _, manifest := range m.Manifests {
-				fmt.Println(manifest.Size)
+			if err := collect(v.Spec.Exporter.Image, dm); err != nil {
+				panic(err)
 			}
-
-			sz := m.Config.Size
-			for _, layer := range m.Layers {
-				sz += layer.Size
+			if err := collect(v.Spec.InitContainer.Image, dm); err != nil {
+				panic(err)
 			}
-			fmt.Println(sz)
-			break
 		}
 		return nil
 	})
 	if err != nil {
 		panic(err)
 	}
+}
+
+func collect(ref string, dm map[string]int) error {
+	data, err := crane.Manifest(ref)
+	if err != nil {
+		return err
+	}
+
+	var m ImageManifest
+	err = json.Unmarshal(data, &m)
+	if err != nil {
+		return err
+	}
+
+	for _, manifest := range m.Manifests {
+		dm[manifest.Digest] = manifest.Size
+	}
+
+	dm[m.Config.Digest] = m.Config.Size
+	for _, layer := range m.Layers {
+		dm[layer.Digest] = layer.Size
+	}
+	return nil
 }
